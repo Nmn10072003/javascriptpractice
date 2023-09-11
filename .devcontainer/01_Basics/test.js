67b5c480f24cd6ed5a48abe27e62accced7c6f7a
Nmn10072003 <131520467+Nmn10072003@users.noreply.github.com>
2023-09-11 17:55:50 +0000

    fghfdhfg

--- .devcontainer/01_Basics/test.js
+++ .devcontainer/01_Basics/test.js
@@ -1,5 +1,13 @@
-console.log("Hello World");
-const nam = "chai and coffee"
-let age = 19
-var course = "idk"
-console.table([nam, age, course])
+// console.log("Hello World");
+// const nam = "chai and coffee"
+// let age = 19
+// var course = "idk"
+// console.table([nam, age, course])
+
+const str = "Hello world this is js practice"
+console.log(str.substring(2,5))
+console.log(str.slice(-15,18))
+console.log(str.trim())
+console.log(str.replace("Hello", "Namaste"))
+console.log(str.split(" "))
+
