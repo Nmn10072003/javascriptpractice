6a8848f69037011216c235ebef6c5988eb3515e0
Nmn10072003 <131520467+Nmn10072003@users.noreply.github.com>
2023-09-12 08:16:53 +0000

    data and time

---
+++ date_and_time.js
@@ -0,0 +1,15 @@
+// const myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleTimeString())
+
+
+// const currentDate = new Date("12-01-2003")
+// console.log(currentDate)
+
+const time = Date.now()
+console.log(time)
+
