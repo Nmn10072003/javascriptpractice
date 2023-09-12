fc7214d4c341935efb3c7e59fa53bfa7df1952c0
Nmn10072003 <131520467+Nmn10072003@users.noreply.github.com>
2023-09-12 06:17:12 +0000

    num and maths

---
+++ practice2.js
@@ -0,0 +1,12 @@
+// const num = 1000000000000
+// console.log(num.toString())
+// console.log(num.toFixed(2))
+// console.log(num.toPrecision(2))
+// console.log(num.toLocaleString('en-
+
+// console.log(Math.abs(-5))
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.1))
+// console.log(Math.floor(4.9))
+
+console.log(Math.floor((Math.random()*10)))
